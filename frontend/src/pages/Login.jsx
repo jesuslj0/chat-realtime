4312c7145bec1352371a1af089115e608dcad13a
jesus <jesuslopj0@gmail.com>
2025-02-27 03:24:10 +0100

    Iniciando frontend con React y Bootstrap

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Container, Card, Button, Form } from "react-bootstrap"
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const response = await fetch("http://localhost:4001/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Error al iniciar sesión");
+      }
+
+      localStorage.setItem("token", data.token);
+      setError(null)
+      console.log("Login exitoso!");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+        { error && <div className="p-3 text-danger">{error}</div>}
+      <Card className="p-4 shadow">
+        <h2 className="text-center mb-3">Iniciar Sesión</h2>
+        <Form onSubmit={(e) => handleSubmit(e)}>
+          <Form.Group className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Ingresa tu email" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Ingresa tu contraseña" />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="w-100">
+            Entrar
+          </Button>
+        </Form>
+      </Card>
+    </Container>
+  );
+};
